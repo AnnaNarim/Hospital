ddd9ce32df945724561718eab4c8b55109b9fbe2
anna_narimanyan@edu.aua.am <anna_narimanyan@edu.aua.am>
2018-12-11 03:11:35 +0400

    almost finished missing update and treatBoth

--- db.js
+++ db.js
@@ -18,7 +18,10 @@ client.connect(function(err) {
 ////////////////////// get All 
 Database.getAllDepart = () => new Promise((resolve, reject) => {
   client.query(
-    'Select * from doctor, ( select * from department d, head_doctors h where d.dept_id = h.dept_id) t1 where doctor.doct_id = t1.doct_id', 
+    `Select doctor.* , t1.name, t1.budget
+    from doctor, ( select d.* , h.doct_id from department d, head_doctors h
+     where d.dep_id = h.dep_id) t1 
+    where doctor.doct_id = t1.doct_id`, 
     (err, res) => {
     if (err) return reject(err);
     return resolve(res.rows);
@@ -92,19 +95,11 @@ Database.getDoctor = (id) => new Promise((resolve, reject) => {
 
 
 /////////////////////// ADD
-Database.addDoc = (text) => {
-  const fn = text.fn;
-  const ln = text.ln; 
-  const salary= text.salary;
-  const birth= text.birth;
-  const dep_id= text.dep_id;
-  const address= text.address;
-  const room= text.room;
-
+Database.addDoc = (firstname, lastname, salary, birth, address, dep_id, room) => {
   return new Promise((resolve, reject) => {
     client.query(`insert into doctor
       (first_name, last_name, salary, birth, dep_id, address, room)
-      values('${fn}', '${ln}', '${salary}', '${birth}', '${dep_id}', '${address}', '${room}')`,
+      values('${firstname}', '${lastname}', '${salary}', '${birth}', '${dep_id}', '${address}', '${room}')`,
       (err, res) => {
       if (err) return reject(err);
       return resolve(res);
@@ -117,7 +112,7 @@ Database.addDoc = (text) => {
 ////////////////////Delete
 Database.deleteDoc = (id) => {
   return new Promise((resolve, reject) => {
-    client.query(`delete from doctor where id=${id}`, (err, res) => {
+    client.query(`delete from doctor where doct_id=${id}`, (err, res) => {
       if (err) return reject(err);
       return resolve(res);
     });
@@ -144,7 +139,7 @@ Database.getAllTreatByDoc = (id) => {
   return new Promise((resolve, reject) => {
     client.query(`select * 
       from doctor, treatment
-      where doctor.doc_id = treatment.doc_id and doctor.doc_id = ${id}`, (err, res) => {
+      where doctor.doct_id = treatment.doct_id and doctor.doct_id = ${id}`, (err, res) => {
       if (err) return reject(err);
       return resolve(res);
     })
@@ -168,13 +163,13 @@ Database.getAllTreatByBoth = (pat_id, doc_id) => {
       (select * from doctor, (select * 
         from treatment
         where treatment.pat_id = ${pat_id}
-          and treatment.doc_id = ${doc_id}) t1
-      where doctor.doc_id = t1.doc_id)
+          and treatment.doct_id = ${doc_id}) t1
+      where doctor.doct_id = t1.doct_id)
       union 
       (select * from patient, (select * 
         from treatment
         where treatment.pat_id = ${pat_id}
-          and treatment.doc_id = ${doc_id}) t1
+          and treatment.doct_id = ${doc_id}) t1
       where patient.pat_id = t1.pat_id)
       `,
       (err, res) => {
@@ -185,3 +180,16 @@ Database.getAllTreatByBoth = (pat_id, doc_id) => {
 };
 
 module.exports = Database;
+
+
+// (select * from doctor, (select * 
+//         from treatment
+//         where treatment.pat_id = 1
+//           and treatment.doct_id = 4) t1
+//       where doctor.doct_id = t1.doct_id)
+//       union 
+//       (select * from patient, (select * 
+//         from treatment
+//         where treatment.pat_id = 1
+//           and treatment.doct_id = 4) t1
+//       where patient.pat_id = t1.pat_id)
--- handlers/hospital.js
+++ handlers/hospital.js
@@ -19,38 +19,42 @@ hospital.getAllDocsCount = () => db.getAllDocsCount();
 hospital.getMaxSalary = () => db.getMaxSalary();
 
 hospital.getDoctor = (request) => {
-  const { id } = request.payload;
+  const { id } = request.params;
   return db.getDoctor(id);
 }
 
 hospital.addDoctor = (request) => {
-  const { text } = request.payload;
-  return db.addDoc(text);
+  const { firstname, lastname, salary, birth, address, dep_id, room} = request.payload;
+  return db.addDoc(firstname, lastname, salary, birth, address, dep_id, room);
 };
 
 hospital.deleteDoctor = (request) => {
-  const { id } = request.payload;
+  const { id } = request.params;
   return db.deleteDoc(id);
 }
 
 ///////////////////////////////// departments
 
-hospital.getAllDepart = () => db.getAllDepart();
+hospital.getAllDepart = () => {
+  console.log('---ekav hasav')
+  return db.getAllDepart();
+} 
 
 ///////////////////////////////// Treatments
 
 hospital.getAllTreatByBoth =(request) => {
-  const { pat_id, doc_id } = request.payload;
+  const { pat_id, doc_id } = request.params;
   return db.getAllTreatByBoth(pat_id, doc_id);
 }
 
 hospital.getAllTreatByDoc =(request) => {
-  const { id } = request.payload;
+  const { id } = request.params;
+  console.log('---oarams', id);
   return db.getAllTreatByDoc(id);
 }
 
 hospital.getAllTreatByPat =(request) => {
-  const { id } = request.payload;
+  const { id } = request.params;
   return db.getAllTreatByPat(id);
 }
 
